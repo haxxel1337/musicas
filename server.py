11ff92852889c8464ae710d1b945b3be06a4af3b
haxxel1337 <65605299+haxxel1337@users.noreply.github.com>
2025-05-25 14:27:57 +0200

    Update server.py

--- server.py
+++ server.py
@@ -60,3 +60,9 @@ def tracks():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
